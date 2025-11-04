6ceb925185ff2499de493fe0057c7140c7e2ccbb
Samuel Schneweiss <schneweisscavalcanti@gmail.com>
2025-11-03 22:55:54 -0300

    interface changes

--- js/portfolio.js
+++ js/portfolio.js
@@ -2,21 +2,24 @@ document.addEventListener("DOMContentLoaded", () => {
     const projects = [
         {
             id: '1',
-            nome: 'Conversor de Binario',
-            alt: 'Conversor de Binario',
-            resumo: 'Plataforma completa de vendas online, com painel administrativo e sistema de pagamento integrado.',
+            nome: 'Conversor de Binário',
+            alt: 'Conversor de Binário',
+            resumo: 'Ferramenta interativa que converte números em binário. Desenvolvida para praticar lógica de programação e manipulação de DOM com JavaScript puro',
+            github: 'https://github.com/jamaralm/binaryConverter'
         },
         {
             id: '2',
             nome: 'TAG Paramentação',
             alt: 'TAG Paramentação',
-            resumo: 'Plataforma web desenvolvida para a empresa TAG Paramentação, voltada à gestão e divulgação de vestimentas hospitalares. O sistema conta com um site institucional moderno e responsivo e um backend robusto em Node.js e Express, garantindo integração entre dados, produtos e área administrativa com foco em desempenho e usabilidade.',
+            resumo: 'Plataforma web desenvolvida para a empresa TAG Paramentação, focada na gestão e divulgação de vestimentas hospitalares. O sistema apresenta um site institucional moderno, totalmente responsivo e desenvolvido com React.',
+            github: 'https://github.com/gustavolinodev/Tag-Site'
         },
         {
             id: '3',
-            nome: 'App de Delivery Local (Mobile)',
-            alt: 'App de Delivery Local',
-            resumo: 'Solução móvel para conectar vendedores e consumidores em uma determinada região, otimizando entregas.',
+            nome: 'Mini Spotify',
+            alt: 'Mini Spotify',
+            resumo: 'Um projeto em Java inspirado no Spotify, desenvolvida para controle de playlists. O projeto permite ao usuário criar, e gerenciar músicas em uma interface moderna e responsiva.',
+            github: 'https://github.com/jamaralm/spotifyProject'
         }   
     ]
 
@@ -27,6 +30,9 @@ document.addEventListener("DOMContentLoaded", () => {
             <article class="card">
                 <h3>${project.nome}</h3>
                 <p>${project.resumo}</p>
+                <button class="mini-bio-btn" onclick="window.open('${project.github}', '_blank')">
+                    Ver no GitHub
+                </button>
             </article>
         `
     }
